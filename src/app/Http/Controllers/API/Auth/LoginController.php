b02f2c498b993520bfb6fc2d5ec423954b5a8c56
panpanYuya <yuya@sanbakaraisu.com>
2022-10-03 21:54:30 +0900

    ログイン認証の処理を追加

--- src/app/Http/Controllers/API/Auth/LoginController.php
+++ src/app/Http/Controllers/API/Auth/LoginController.php
@@ -6,13 +6,23 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\API\Auth\LoginRequest;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
+
+
     public function authenticate(LoginRequest $request): JsonResponse{
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            //TODO正常に動く用になった時にもどす
+            // $request->session()->regenerate();
+            return  response()->json([
+             Auth::user()
+            ]);
+        }
+
         return response()->json([
-            'result' => true,
+            'result' => false,
         ]);
     }
 }
