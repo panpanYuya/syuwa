b04b601a48963c83799b80c4740814544a88d50c
panpanYuya <yuya@sanbakaraisu.com>
2022-10-04 16:40:06 +0900

    データベースのコメントを追加

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name')->comment('ユーザーネーム');
+            $table->string('email')->unique()->comment('メールアドレス');
+            $table->string('password')->comment('パスワード');
             $table->timestamps();
         });
     }
