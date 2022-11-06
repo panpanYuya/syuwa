fc9b9ac6dc998dcc27893e100679a887a1e86403
panpanYuya <yuya@sanbakaraisu.com>
2022-11-06 18:31:03 +0900

    logoを表示するためのcomponentを作成

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -5,21 +5,25 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from '../material/material.module';
 import { AuthModule } from './auth/auth.module';
-import { BoardComponent } from './drink/board/board.component';
 import { HeaderComponent } from './common/header/header.component';
+import { LogoComponent } from './common/logo/logo.component';
 import { NavigationComponent } from './common/navigation/navigation.component';
+import { BoardComponent } from './drink/board/board.component';
 
 @NgModule({
   declarations: [
     BoardComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    LogoComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     MaterialModule,
-    AuthModule
   ],
+  exports: [
+    LogoComponent
+  ]
 })
 export class PagesModule { }
